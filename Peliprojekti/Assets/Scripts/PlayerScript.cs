f0a6f4c63100ca7f2be5bd7b8c61f4ea8a5ef014
Salokoski Ville <ovisa007@edu.mamk.fi>
2016-07-21 12:34:54 +0300

    Added player and weapon prefabs

---
+++ Peliprojekti/Assets/Scripts/PlayerScript.cs
@@ -0,0 +1,103 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerScript : MonoBehaviour
+{
+
+    private Vector2 movement;
+    private Rigidbody2D rigidbodyComponent;
+    public Vector3 speed2 = new Vector2(10, 10);
+    float inputX;
+    float inputY;
+    bool walking;
+    Animator anim;
+
+
+    // Use this for initialization
+    void Start()
+    {
+
+        anim = GetComponent<Animator>();
+
+
+        rigidbodyComponent = GetComponent<Rigidbody2D>();
+    }
+
+    
+
+    void FixedUpdate()
+    {
+
+        
+            
+
+
+
+
+
+
+
+
+        rigidbodyComponent.AddForce(movement * 4);
+
+    }
+
+
+    // Update is called once per frame
+    void Update()
+    {
+        //WASD CONTROLS
+        inputX = 0;
+        inputY = 0;
+
+        if (Input.GetKey("w"))
+        {
+            inputY = 1;
+            anim.SetFloat("Speed", Mathf.Abs(2.0F));
+
+        }
+
+        if (Input.GetKey("a"))
+        {
+            inputX = -1;
+            anim.SetFloat("Speed", Mathf.Abs(2.0F));
+        }
+
+        if (Input.GetKey("s"))
+        {
+            inputY = -1;
+            anim.SetFloat("Speed", Mathf.Abs(2.0F));
+        }
+
+        if (Input.GetKey("d"))
+        {
+            inputX = 1;
+            anim.SetFloat("Speed", Mathf.Abs(2.0F));
+        }
+
+        movement = new Vector2(speed2.x * inputX, speed2.y * inputY);
+
+        //-anim
+
+
+       
+
+      
+
+
+     if(Input.GetKey("w")==false && Input.GetKey("a") == false && Input.GetKey("s") == false && Input.GetKey("d") == false)
+        {
+            anim.SetFloat("Speed", 0);
+        }
+            
+        
+
+
+
+
+
+
+    }//update
+
+
+}
